f6810425bbdaa389d2ba6e5618d837e7add13051
mArTi3 <pro31@rambler.ru>
2026-06-12 11:59:04 +0300

    feat: wire RetryConfig into resolver factory from AppSettings

--- src/purl_resolver/resolver/factory.py
+++ src/purl_resolver/resolver/factory.py
@@ -6,6 +6,7 @@ from .ecosystems import EcosystemsResolver
 from .interface import Resolver
 from .librariesio import LibrariesIoResolver
 from .purl2repo import Purl2RepoResolver
+from .retry import RetryConfig
 
 
 def build_resolvers(
@@ -21,12 +22,20 @@ def build_resolvers(
             cache_dir=settings.cache_dir,
         ),
     ]
+    retry_config = RetryConfig(
+        max_attempts=app_settings.retry_max_attempts,
+        base_cooldown_seconds=app_settings.retry_base_cooldown_seconds,
+    )
     if app_settings.ecosystems_enabled:
         resolvers.append(
-            EcosystemsResolver(api_key=app_settings.ecosystems_api_key)
+            EcosystemsResolver(
+                api_key=app_settings.ecosystems_api_key,
+                max_requests_per_second=app_settings.ecosystems_max_requests_per_second,
+                retry_config=retry_config,
+            )
         )
     if app_settings.librariesio_enabled and app_settings.librariesio_api_key:
         resolvers.append(
-            LibrariesIoResolver(api_key=app_settings.librariesio_api_key)
+            LibrariesIoResolver(api_key=app_settings.librariesio_api_key, retry_config=retry_config)
         )
     return resolvers
